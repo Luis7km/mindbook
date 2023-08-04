649d2987a335199e40a61edffd693368dd044057
Luis7km <anthony.milla1997@gmail.com>
2023-08-03 19:20:37 -0500

    Se seteo una navegacion basica entre las paginas

--- components/LeftBar/leftBar.jsx
+++ components/LeftBar/leftBar.jsx
@@ -1,13 +1,59 @@
 import React from "react";
 import styles from './leftBar.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from 'next/router'; // Importa el hook useRouter
 
 export default function LeftBar() {
-    return(
+    const router = useRouter(); // Obtén la instancia de useRouter
+
+    return (
         <div className={styles.options}>
-            <button className={styles.button}>Inicio</button>
-            <button className={styles.button}>Perfil</button>
-            <button className={styles.button}>Mensajes</button>
-            <button className={styles.button}>Juegos</button>
+            {/* Aplica estilos condicionales al enlace dependiendo de la ruta actual */}
+            <Link href='/' className={router.pathname === '/' ? styles.buttonSelected : styles.button}>
+                <Image
+                    priority
+                    src={router.pathname === '/' ? "/images/homeSelected.svg" : "/images/home.svg"}
+                    className={styles.image}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+                Inicio
+            </Link>
+            <Link href='/profile' className={router.pathname === '/profile' ? styles.buttonSelected : styles.button}>
+                <Image
+                    priority
+                    src={router.pathname === '/profile' ? "/images/profileSelected.svg" : "/images/profile.svg"}
+                    className={styles.image}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+                Perfil
+            </Link>
+            <Link href='/messages' className={router.pathname === '/messages' ? styles.buttonSelected : styles.button}>
+                <Image
+                    priority
+                    src={router.pathname === '/messages' ? "/images/messageSelected.svg" : "/images/message.svg"}
+                    className={styles.image}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+                Mensajes
+            </Link>
+            <Link href='/games' className={router.pathname === '/games' ? styles.buttonSelected : styles.button}>
+                <Image
+                    priority
+                    src={router.pathname === '/games' ? "/images/gameSelected.svg" : "/images/game.svg"}
+                    className={styles.image}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+                Juegos
+            </Link>
         </div>
     )
 }
